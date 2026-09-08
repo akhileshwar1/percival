2711d759d775cb379a17d29911d0bf41a15a799c
Akhil Kandi <akhilprm999@gmail.com>
2026-09-08 16:57:03 +0530

    Add run_all.sh to init all strategies and run for a day

---
+++ run_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+./test_server_eq_100.sh
+./test_server_eq_200.sh
+./test_server_eq_500.sh
+# ./test_server_eq_opp.sh
+./test_server_fno_india.sh
+./test_server_fno_india_beta.sh
+./test_server_fno_india_combined.sh
+./test_server_fno_india_levered.sh
+./test_server_Ramani_FNO.sh
+# ./test_server_us_ramani.sh
+# ./test_server_us_sp.sh
+# ./test_server_us_sp_global.sh
+# ./test_server_us_spx.sh
+#
+./test_server_day_7_apr.sh
